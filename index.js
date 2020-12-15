77e85f6cf994685304c56fa21f54144e849334b0
bignami <bignami>
2020-12-15 22:02:50 +0900

    pirnt qureystring

--- index.js
+++ index.js
@@ -32,21 +32,45 @@ const server = net.createServer(function(socket) {
         const methodAndResource =data.substring(0,data.indexOf("HTTP")).split(" ") ;
         const method = methodAndResource[0];
         const resource = methodAndResource[1];
-        let path;
-        let param;
+        let path = "";
+        let param ="";
+        let key = "";
+        let value = "";
+        let paramObj = {};
 
         if(resource.indexOf("?")>0){
             path = resource.substring(0,resource.indexOf("?"));
             param = resource.substring(resource.indexOf("?")+ 1);
             console.log(path, param,"PATH랑 Param");
+            key = param.substring(0,param.indexOf("="));
+            value = param.substring(param.indexOf("=")+1);
+
+            paramObj.key = value;
+            
+            
+        }
+        else {
+            path = resource;
         }
         
         if(method === 'GET'){
-            switch(resource){
+            switch(path){
                 case "/":
                     socket.write(res);
+                    console.log(path);
                     break;
                 
+                case "/Profile":
+                    socket.write("HTTP/1.1 200 OK\r\n"+
+                    "Content-Type: text/html\r\n"+
+                    "Content-Length: 14\r\n"+
+                    "\r\n"
+                    +paramObj.key+" welcome");
+                    break;
+
+                case "/calculator":
+                    socket.write(res);
+                    break;
                 default :
                     socket.write(err);
                     break;
